85d690620befb535e1a63da67ffd276ebdd97e31
Michael Gourlay <michael.gourlay@hotmail.com>
2020-05-31 14:19:34 +1000

    Added Scheduling

---
+++ src/server/DeleteBillboardRequest.java
@@ -0,0 +1,19 @@
+package server;
+
+import java.io.Serializable;
+
+public class DeleteUserRequest implements Serializable {
+
+
+    private String username;
+
+
+    public DeleteUserRequest(String username) {
+        this.username = username;
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+}
